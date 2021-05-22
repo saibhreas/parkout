57172f492413c529813819080df7ae06cc07b336
C. Rando <crando@gmail.com>
2021-05-22 18:48:34 -0400

    finalizing update

--- assets/scriptFiles/logic.js
+++ assets/scriptFiles/logic.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     var alerts = $("#alerts").on("click",);
     console.log(alerts);
 
-//
+    //
     //button click 
 
 
@@ -21,140 +21,156 @@ $(document).ready(function () {
     var  = $('');
     var timeDispEl = $("#time-display");
     var apiKey = "653094733b20fc02dc6f1e6e6b8bf37e";
-    
+
     // TODO declare & troubleshoot
     // TODO: GLOBAL VARIABLES
     var chosenCity = $("#chosen-city");
 
-    function npsApiCall() {
-        $.ajax({
-            url: "https://developer.nps.gov/api/v1/parks?parkCode=&stateCode=NJ&api_key=wuCQVj3eIqguFjOi2HEN7r2l4IDFb4FD90hGt35N",
-            success: function (response) {
-                console.log(response);
-                var latitude = response.data[0].latitude;
-                var longitude = response.data[0].longitude;
-                console.log(latitude, longitude);
-                mapQuestApiCall(latitude, longitude);
-            }
-        })
-    };
-    npsApiCall();
-
-    //*! LAT & LONGITUDE
-    function mapQuestApiCall(x, y) {
-        console.log(x, y)
-        var mapUrl = "https://www.mapquestapi.com/staticmap/v5/map?key=" + MAPQ_API_KEY + "&center=" + x + "," + y + "&zoom=10&type=hyb&size=600,400@2x";
-        console.log(mapUrl)
-    };
-    // *? ALL ABOVE ARE ORIGINAL TO PARKOUT
-
-
-    // *! COMPLETED gets selected city CURRENT weather
-    function getCurrentPark(event) {
-        event.preventDefault();
-        var city = $("#city-name").val();
-        console.log("current searched was: ", city);
-        $("#chosen-city").text(city);
-        $("#current-city").text(city);
-        var currentUrlApi = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-        console.log("current day communication succeed: ", currentUrlApi);
-        $.ajax({
-            url: currentUrlApi,
-            success: function (response) {
-                console.log("object extracted: ", response);
-                console.log("attempting to pull the json: ", response.weather[0].icon);
-                console.log("this is the lat, then lon: ", response.coord.lat, response.coord.lon);
-                getFiveDayWeatherApi(response.coord.lat, response.coord.lon);
-            },
-            error: function (xhr, status, error) {
-                console.log("status: ", status)
-                console.log("error: ", error)
-            },
-            complete: function (xhr, status) {
-                console.log("complete: ", status)
-            }
-        })
-    };
-    // TODO must return null if city is invalid
+    function npsApiCall(parkNJ) {
+        var npsKey = "aKdQbl5YRDOdOcAzaiDfbacSBby5NQWEU8s5Mi5D";
+        var npsUrl = `https://developer.nps.gov/api/v1/parks?stateCode=${parkNJ}&api_key=${npsKey}`
+            console.log("my park list: ", npsUrl);
+$.ajax({
+    url: npsUrl,
+    success: function (response) {
+        console.log(response.data);
+        $('#myList').empty();
+        for (let i = 0; i < response.data.length; i++) {
+            var nameP = response.data[i].fullName;
+            var latP = response.data[i].latitude;
+            var longP = response.data[i].longitude;
+            $('#myList').append('<a class=" dropdown-item" href=" #">' + nameP + '</a>')
+        }
+        // var latitude = response.data[0].latitude;
+        // var longitude = response.data[0].longitude;
+        // console.log(latitude, longitude);
+        //mapQuestApiCall(latitude, longitude);
+    },
+    error: function (xhr, status, error) {
+        console.log("status: ", status)
+        console.log("error: ", error)
+    },
+    complete: function (xhr, status) {
+        console.log("complete: ", status)
+    }
+})
 
 
-    // *! COMPLETED gets 5 day weather forecast
-    function getFiveDayWeatherApi(lat, lon) {
-        var city = $("#city-name").val();
-        console.log("current searched was: ", city)
-        var fiveDayUrlApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,current,alerts&units=imperial&appid=${apiKey}`
-        console.log("fivedayURL: ", fiveDayUrlApi)
-        $.ajax({
-            url: fiveDayUrlApi,
-            success: function (response) {
-                console.log("object extracted: ", response);
-                console.log(response.daily[0].dt);
-                for (var i = 0; i < 6; i++) {
-                    var weather = response.daily[i].weather[0].description;
-                    var icon = response.daily[i].weather[0].icon;
-                    var temp = response.daily[i].temp.day;
-                    var uvi = response.daily[i].uvi;
-                    var iconImage = $("<img>").attr("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
-                    console.log(iconImage)
-                    console.log(response.daily[i].temp.day);
-                    console.log("div" + '[data-index="' + i + '"]')
-                    $("div" + '[data-index="' + i + '"]').append(
-                        " weather: " + weather,
-                        iconImage, "<br>",
-                        " temp: " + temp, "<br>",
-                        " uv index: " + uvi
-                    );
-                    console.log(uvi)
-                    // TODO fetch on uv index then displays color to page
+//*! LAT & LONGITUDE
+function mapQuestApiCall(x, y) {
+    console.log(x, y)
+    var mapUrl = "https://www.mapquestapi.com/staticmap/v5/map?key=" + MAPQ_API_KEY + "&center=" + x + "," + y + "&zoom=10&type=hyb&size=600,400@2x";
+    console.log(mapUrl)
+};
+// *? ALL ABOVE ARE ORIGINAL TO PARKOUT
 
-                    // if (uvi <= 2) {
-                    //     document.getElementsByTagName("H1")[0].setAttribute("class", "democlass");
-                    //     uvInd.css({ "background-color": "green" })
-                    // } else if (uvi <= 5) {
-                    //     uvInd.css({ "background-color": "yellow", "color": "black" })
-                    // } else if (uvi <= 7) {
-                    //     uvInd.css({ "background-color": "orange" })
-                    // } else if (uvi <= 10) {
-                    //     uvInd.css({ "background-color": "red" })
-                    // } else if (uvi >= 11) {
-                    //     uvInd.css({ "background-color": "purple" })
-                    // }
-                    storeObj = {
-                        weather: weather,
-                        icon: icon,
-                        temp: temp,
-                        uvi: uvi,
-                    };
-                    //*! COMPLETED must store value entered by userinput to user storage
-                    localStorage.setItem(city + " day-" + i, JSON.stringify(storeObj));
+
+// *! COMPLETED gets selected city CURRENT weather
+function getCurrentPark(event) {
+    event.preventDefault();
+    var city = $("#city-name").val();
+    console.log("current searched was: ", city);
+    $("#chosen-city").text(city);
+    $("#current-city").text(city);
+    var currentUrlApi = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    console.log("current day communication succeed: ", currentUrlApi);
+    $.ajax({
+        url: currentUrlApi,
+        success: function (response) {
+            console.log("object extracted: ", response);
+            console.log("attempting to pull the json: ", response.weather[0].icon);
+            console.log("this is the lat, then lon: ", response.coord.lat, response.coord.lon);
+            getFiveDayWeatherApi(response.coord.lat, response.coord.lon);
+        },
+        error: function (xhr, status, error) {
+            console.log("status: ", status)
+            console.log("error: ", error)
+        },
+        complete: function (xhr, status) {
+            console.log("complete: ", status)
+        }
+    })
+};
+// TODO must return null if city is invalid
+
+
+// *! COMPLETED gets 5 day weather forecast
+function getFiveDayWeatherApi(lat, lon) {
+    var city = $("#city-name").val();
+    console.log("current searched was: ", city)
+    var fiveDayUrlApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,current,alerts&units=imperial&appid=${apiKey}`
+    console.log("fivedayURL: ", fiveDayUrlApi)
+    $.ajax({
+        url: fiveDayUrlApi,
+        success: function (response) {
+            console.log("object extracted: ", response);
+            console.log(response.daily[0].dt);
+            for (var i = 0; i < 6; i++) {
+                var weather = response.daily[i].weather[0].description;
+                var icon = response.daily[i].weather[0].icon;
+                var temp = response.daily[i].temp.day;
+                var uvi = response.daily[i].uvi;
+                var iconImage = $("<img>").attr("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
+                console.log(iconImage)
+                console.log(response.daily[i].temp.day);
+                console.log("div" + '[data-index="' + i + '"]')
+                $("div" + '[data-index="' + i + '"]').append(
+                    " weather: " + weather,
+                    iconImage, "<br>",
+                    " temp: " + temp, "<br>",
+                    " uv index: " + uvi
+                );
+                console.log(uvi)
+                // TODO fetch on uv index then displays color to page
+
+                // if (uvi <= 2) {
+                //     document.getElementsByTagName("H1")[0].setAttribute("class", "democlass");
+                //     uvInd.css({ "background-color": "green" })
+                // } else if (uvi <= 5) {
+                //     uvInd.css({ "background-color": "yellow", "color": "black" })
+                // } else if (uvi <= 7) {
+                //     uvInd.css({ "background-color": "orange" })
+                // } else if (uvi <= 10) {
+                //     uvInd.css({ "background-color": "red" })
+                // } else if (uvi >= 11) {
+                //     uvInd.css({ "background-color": "purple" })
+                // }
+                storeObj = {
+                    weather: weather,
+                    icon: icon,
+                    temp: temp,
+                    uvi: uvi,
                 };
-            },
-            error: function (xhr, status, error) {
-                console.log("status: ", status)
-                console.log("error: ", error)
-            },
-            complete: function (xhr, status) {
-                console.log("complete: ", status)
-            }
-        })
-    };
-    // *! COMPLETED time display function
-    function displayTime() {
-        var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-        timeDispEl.text(rightNow);
-        var present = rightNow.substring(0, 6);
-        console.log(present)
-    };
+                //*! COMPLETED must store value entered by userinput to user storage
+                localStorage.setItem(city + " day-" + i, JSON.stringify(storeObj));
+            };
+        },
+        error: function (xhr, status, error) {
+            console.log("status: ", status)
+            console.log("error: ", error)
+        },
+        complete: function (xhr, status) {
+            console.log("complete: ", status)
+        }
+    })
+};
+// *! COMPLETED time display function
+function displayTime() {
+    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDispEl.text(rightNow);
+    var present = rightNow.substring(0, 6);
+    console.log(present)
+};
 
-    $(searchParkBtnEl).click(function () {
-        $("#current-weather-container").empty();
-        $("#day-one-container").empty();
-        $("#day-two-container").empty();
-        $("#day-three-container").empty();
-        $("#day-four-container").empty();
-        $("#day-five-container").empty();
-    });
+$(searchParkBtnEl).click(function () {
+    $("#current-weather-container").empty();
+    $("#day-one-container").empty();
+    $("#day-two-container").empty();
+    $("#day-three-container").empty();
+    $("#day-four-container").empty();
+    $("#day-five-container").empty();
+});
 
-    setInterval(displayTime, 1000);
-    searchParkBtnEl.on("click", getCurrentWeather);
+setInterval(displayTime, 1000);
+searchParkBtnEl.on("click", getCurrentWeather);
 });
